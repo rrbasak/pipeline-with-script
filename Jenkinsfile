692cf72973be1a57f71a3e111bea373fd59d12f4
Rajdeep Basak <rajdeepbasak2000@gmail.com>
2025-04-08 22:24:47 +0530

    Initial commit

---
+++ Jenkinsfile
@@ -0,0 +1,15 @@
+pipeline{
+    agent any
+    stages{
+        stage('compile') {
+            steps {
+                bat 'javac Test.java'
+            }
+        }
+        stage('run') {
+            steps {
+                bat 'java Test'
+            }
+        }
+    }
+}
---
+++ Test.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Test {
+    public static void main(String[] args) {
+        System.err.println("Hello, Jenkins!");
+        System.out.println("how are you?");
+        System.out.println("I am fine, thank you!");
+        System.out.println("I am learning Jenkins!");
+        System.out.println(new Date());  
+        System.out.println("I am learning Jenkins! 2nd time");
+    }
+}
