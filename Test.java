c0cc1f8252ed19727a13fd71af216e85ccdeddcb
Rajdeep Basak <rajdeepbasak2000@gmail.com>
2025-04-08 22:30:45 +0530

    initial commit

--- Test.java
+++ Test.java
@@ -2,11 +2,6 @@ import java.util.Date;
 
 public class Test {
     public static void main(String[] args) {
-        System.err.println("Hello, Jenkins!");
-        System.out.println("how are you?");
-        System.out.println("I am fine, thank you!");
-        System.out.println("I am learning Jenkins!");
-        System.out.println(new Date());  
-        System.out.println("I am learning Jenkins! 2nd time");
+        System.err.println("Hello, Jenkins! with pipeline");
     }
 }
